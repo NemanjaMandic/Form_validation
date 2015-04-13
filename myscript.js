8ab6653be606f4d8dadf7d60b25f35fed2d6c9ee
Nemanja Mandic <nemanjans84@gmail.com>
2015-04-13 18:21:17 +0200

    Generic validation by tagname'

--- myscript.js
+++ myscript.js
@@ -1,15 +1,22 @@
-var myField = document.theform.mytelephone;
-var myError = document.getElementById('formerror');
+var inputFields = document.theform.getElementsByTagName("input");
+
+for(key in inputFields){
+  var myField = inputFields[key];
+  var myError = document.getElementById('formerror');
 
 myField.onchange = function(){
-	var myPattern = new RegExp("\\d{3}[\\-]\\d{3}[\\-]\\d{4}", "i");
+	var myPattern = this.pattern; //in forma.html pattern="\d{3}[\-]\d{3}[\-]\d{4}
+	var myPlaceholder = this.placeholder;
 	var isValid = this.value.search(myPattern) >= 0;
 
 	if(!(isValid)){
-   myError.innerHTML = "Input does not match expected pattern. xxx-xxx-xxxx";
+   myError.innerHTML = "Input does not match expected pattern. " + myPlaceholder;
 	}else{
 	   myError.innerHTML = "";
 	}
 }
+
+}
+
 
 
